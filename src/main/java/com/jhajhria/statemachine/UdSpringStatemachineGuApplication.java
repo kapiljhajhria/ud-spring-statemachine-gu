cb2b906dd4ab37238c0338226c9662dc83de8dfa
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-18 10:45:22 +0530

    init

---
+++ src/main/java/com/jhajhria/statemachine/UdSpringStatemachineGuApplication.java
@@ -0,0 +1,13 @@
+package com.jhajhria.statemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdSpringStatemachineGuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdSpringStatemachineGuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhajhria/statemachine/UdSpringStatemachineGuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhajhria.statemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdSpringStatemachineGuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
