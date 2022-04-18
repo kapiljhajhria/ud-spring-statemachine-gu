fb0600d17271d7f706ecb6ba55a01fa60eb89852
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-18 17:40:12 +0530

    Initialize stateMachine from db
    - this is used when we are storing states in db and need to rehydrate our app using that stored state.
    - this is an expensive process and should be avoided if possible.
    - add method to get payment state from db, then stop state machine if its running, then populate state machine based on payment data which we got from db and then start the state machine with that data

--- src/main/java/com/jhajhria/statemachine/services/PaymentServiceImpl.java
+++ src/main/java/com/jhajhria/statemachine/services/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import com.jhajhria.statemachine.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -36,5 +37,24 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
         return null;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+       //get payment from db using the id
+        Payment payment = paymentRepository.findById(paymentId).get();
+        //create a new state machine
+        StateMachine<PaymentState, PaymentEvent> stateMachine = stateMachineFactory.getStateMachine(payment.getId().toString());
+
+        stateMachine.stop();
+
+        stateMachine.getStateMachineAccessor().doWithAllRegions(sma -> {
+            sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(),null,null,null));//set up context for the state machine.
+            // Basically we are telling the state machine to stop, and we are setting state to match the payment data which we got from the db
+        });
+
+        //start the state machine
+        stateMachine.start();
+
+        return stateMachine;
     }
 }
