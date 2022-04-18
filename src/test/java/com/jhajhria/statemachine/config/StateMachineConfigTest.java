f345694a6c80e1769fa97a314011f6d1c11791a4
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-18 15:26:08 +0530

    Set up State Transition and Configuration for it.
    - state transitions added and also what event trigger those transition
    - simple manual test to check those transition.
    - used deprecated method in test so those need to be updated.

---
+++ src/test/java/com/jhajhria/statemachine/config/StateMachineConfigTest.java
@@ -0,0 +1,40 @@
+package com.jhajhria.statemachine.config;
+
+import com.jhajhria.statemachine.domain.PaymentEvent;
+import com.jhajhria.statemachine.domain.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StateMachineConfigTest {
+
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine(){
+        StateMachine<PaymentState, PaymentEvent> stateMachine = factory.getStateMachine(UUID.randomUUID());
+
+        stateMachine.start();
+
+        System.out.println(stateMachine.getState().toString());
+
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+
+        System.out.println(stateMachine.getState().toString());
+
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+
+        System.out.println(stateMachine.getState().toString());
+
+        ///TODO : replace deprecated methods.
+    }
+
+}
