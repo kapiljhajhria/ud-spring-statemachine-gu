baa8a41dc46afea33646032804b7d959430517c0
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-18 21:42:37 +0530

    bugFix: generate id for Payment

--- src/main/java/com/jhajhria/statemachine/domain/Payment.java
+++ src/main/java/com/jhajhria/statemachine/domain/Payment.java
@@ -15,8 +15,9 @@ import java.math.BigDecimal;
 @Entity
 public class Payment {
 
-    @Id
+    @Id()
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Enumerated(EnumType.STRING)
